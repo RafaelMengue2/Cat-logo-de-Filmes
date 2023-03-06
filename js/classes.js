ca4f1bdcc096aea863ad34e121c9bbbc14c534ad
rafael mengue <rafael-dsmengue@educar.rs.gov.br>
2023-03-06 16:56:04 -0300

    Segundo Commit

---
+++ js/classes.js
@@ -0,0 +1,27 @@
+class Ator{
+    constructor(id, nome){
+        this.nome=nome;
+        this.id=id;
+    }
+}
+
+class Diretor{
+    constructor(id, nome){
+        this.nome=nome;
+        this.id=id;
+    }
+}
+
+class Filme{
+    constructor(id, titulo, ano, categoria, cartaz, direcao, atores, classificacao, avaliacao){
+        this.id=id;
+        this.titulo=titulo;
+        this.ano=ano;
+        this.categoria=categoria;
+        this.cartaz=cartaz;
+        this.direcao=direcao;
+        this.atores=atores;
+        this.classificacao=classificacao;
+        this.avaliacao=avaliacao;    
+    }
+}
